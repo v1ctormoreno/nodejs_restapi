30e3113d08732fd828a977d5a1a5cae64a095052
v1ctormoreno <victormorenotin@gmail.com>
2020-05-17 00:04:35 +0200

    added docs at /

--- src/routes/default.routes.js
+++ src/routes/default.routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const pool = require('../database');
 
+const info = require('../info.json');
+
 router.get('/', (req, res) => {
-        res.render(`pages/index`);
+        res.json(info);
 });
 
 module.exports=router; 
